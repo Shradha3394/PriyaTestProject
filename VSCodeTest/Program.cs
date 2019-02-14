abb87afe6af9029b5622015a0c4e538379418dd0
Shradha <sjoshi@ex2india.com>
2019-02-14 15:15:28 +0530

    VSCode Project

---
+++ VSCodeTest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintNoOneToTen();
+           //PrintPattern1();
+           //PrintPattern2();
+           //PrintPattern3();
+        }
+
+        private static void PrintNoOneToTen()
+        {
+            Console.WriteLine("display number one to ten");
+            for(var i=1;i<=10;i++)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine("");
+        }
+
+        //Print Pattern
+        static void PrintPattern1()
+        {
+            for (var i = 1; i <= 3; i++)
+            {
+                for (var j = 1; j <= 3 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (var k = 1; k <= (2 * i) - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+
+        //  1
+        // 234
+        //56789
+        static void PrintPattern2()
+        {
+            var number = 0;
+            
+            for (var i = 1; i <= 3; i++)
+            {
+                for (var j = 1; j <= 3 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (var k = 1;k <=(2 * i)- 1;k++)
+                {
+                    Console.Write("{0}", ++number);
+
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        //*****
+        // ***
+        //  *
+        static void PrintPattern3()
+        {
+            for (var i = 5; i >= 1; i--)
+            {
+                for (var j = 1; j <= 5-i; j++)
+                {
+                    Console.Write(" ");
+                }
+               for(var k = 0; k <= i-2;k++)
+                {
+                    Console.Write(" ");
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
