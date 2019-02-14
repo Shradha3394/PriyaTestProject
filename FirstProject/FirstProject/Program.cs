934d59084ebf38e0d11f184be281c37e7ad2b75c
Priya <prkumar@ex2india.com>
2019-02-14 09:44:09 +0530

    pattern

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -6,54 +6,87 @@ using System.Threading.Tasks;
 
 namespace FirstProject
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			PrintPattern1();
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //PrintPattern1();
+           // PrintPattern2();
+            PrintPattern3();
 
-		}
+        }
 
-		//Print Pattern
-		static void PrintPattern1()
-		{
-			for (var i = 1; i <= 3; i++)
-			{
-				for (var j = 1; j <=3-i; j++)
-				{
-					Console.Write(" ");
-				}
-				for (var k = 1; k <= (2 * i) - 1 ; k++)
-				{
-					Console.Write("*");
-				}
-			    Console.WriteLine("");
+        //Print Pattern
+        static void PrintPattern1()
+        {
+            for (var i = 1; i <= 3; i++)
+            {
+                for (var j = 1; j <= 3 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (var k = 1; k <= (2 * i) - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
 
             }
             Console.ReadLine();
-		}
+        }
 
         //  1
         // 234
         //56789
-	    static void PrintPattern2()
-	    {
+        static void PrintPattern2()
+        {
+            for (var i = 1; i <= 3; i++)
+            {
+                for (var j = 1; j <= 3 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (var k = 1;k<=(2 * i)-1;k++)
+                {
+                    Console.Write("  ");
+                }
 
-	        
-	    }
+                Console.WriteLine("");
+            }
+            
 
 
-	    //*****
-	    // ***
-	    //  *
-	    static void PrintPattern3()
-	    {
+        }
 
 
-	    }
+
+
+
+        //*****
+        // ***
+        //  *
+        static void PrintPattern3()
+        {
+            for (var i = 5; i >= 1; i--)
+            {
+                for (var j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                
+
+
+            }
+              Console.WriteLine("");
+
+
+
+        }
     }
 }
-// Task2:string Reverse
+    
+
+
 
        
      
