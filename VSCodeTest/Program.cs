d25055e10ba78d5b2f4fdb7777472f11045b60ef
Priya <prkumar@ex2india.com>
2019-02-22 09:42:13 +0530

    change code

--- VSCodeTest/Program.cs
+++ VSCodeTest/Program.cs
@@ -10,7 +10,8 @@ namespace FirstProject
            //PrintPattern1();
            //PrintPattern2();
            //PrintPattern3();
-             stringreverse();
+           //stringreverse();
+             printpercentage();
         }
 
         private static void PrintNoOneToTen()
@@ -89,18 +90,68 @@ namespace FirstProject
         static void stringreverse()
         {
             string name=" ",revname=" ";
+            int vowelsCount=0,spaceCount=0;
+            char str;
             Console.WriteLine("enter the string:");
-           name=  Console.ReadLine();
-            for(var i=1;i>= 0;i--)
+            name=  Console.ReadLine();
+            for(int i= name.Length -1;i>= 0;i--)
             {
                 revname=revname +  name[i];
+                //Titlecase                
+                
+                   // name=char.ToUpper(name[0])+name.Substring(1);
+                  
+                
+                 //Console.WriteLine("string with first letter uppercase:[name]");
+                
+               
             }
+            
+            //vowels & space count
+            for( int i=0;i <name.Length;i++)
+            {
+                str=name[i];
+                if(str=='a'||str=='e'||str=='i'||str=='o'||str=='u')
+                {
+                     vowelsCount++;
+                }
+                else if( str==' ')
+                {
+                    spaceCount++;
+                }
+                 
+                
+            }
+
+    
             Console.WriteLine("reverse string is:-{0}" ,revname);
+           // Console.WriteLine("ToUppercase is {0}",ToUpper);
+            Console.WriteLine("No of vowles:{0}",vowelsCount);
+            Console.WriteLine("No of space:{0}",spaceCount);
+
             Console.ReadLine();
 
 
 
+
+
         }
+        static void printpercentage()
+        {
+            
+            Console.Write("enter the number");
+            for(int i=1;i>100;i++)
+            {
+                Console.Write("wrong input:-{0}",i);
+            }   
+               Console.ReadLine();
+
+
+            
+
+
+        }
+        
         
     }
 }
